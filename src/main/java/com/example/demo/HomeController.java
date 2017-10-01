610b999a9b8357441898a3476ae82f7b6c2bcfe9
Huan Wu <huanjiewu@gmail.com>
2017-09-30 21:56:51 -0400

    intial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Autowired
+    ActorRepository actorRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String loadData(){
+        Actor a = new Actor();
+        a.setName("Sandra Bullock");
+        a.setRealname("Sandra Mae Bullowski");
+        actorRepository.save(a);
+
+        Movie movie=new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+        movie.setDescription("A movie that should not exist");
+
+        movie.addActor(a);
+        movieRepository.save(movie);
+
+        movie=new Movie();
+        movie.setTitle("Ironman");
+        movie.setYear(2008);
+        movie.setDescription("About Ironman....");
+
+        movie.addActor(a);
+        movieRepository.save(movie);
+
+        return "redirect:/view";
+
+
+    }
+    @RequestMapping("/view")
+    public String showData(Model model) {
+        model.addAttribute("actors", actorRepository.findAll());
+        return "index";
+    }
+}
